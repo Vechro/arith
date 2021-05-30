0d245032d8a18509c07c221ac6c7ab1637ed6feb
vechro <stenmr@gmail.com>
2021-05-30 13:26:05 +0300

    Set up a basic bot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val kotlinVersion = "1.5.10"
-val spekVersion = "2.0.16"
-val discord4JVersion = "3.1.5"
+val spekVersion = "2.0.15"
 
 plugins {
     kotlin("jvm") version "1.5.10"
@@ -23,14 +22,14 @@ dependencies {
     testImplementation("org.spekframework.spek2:spek-dsl-jvm:$spekVersion")
     testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:$spekVersion")
 
-    // spek requires kotlin-reflect, can be omitted if already in the classpath
     testRuntimeOnly("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
 
-    implementation("com.discord4j:discord4j-core:$discord4JVersion")
+    implementation("com.discord4j:discord4j-core:3.1.5")
+    implementation("org.slf4j:slf4j-simple:1.7.30")
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "8"
+    kotlinOptions.jvmTarget = "1.8"
 }
 
 application {
--- src/main/resources/Env.kt
+++
@@ -1,5 +0,0 @@
-object Env {
-    const val PREFIX = "!"
-    const val TOKEN = ""
-    const val DATA_PATH = "./data/data.json"
-}
